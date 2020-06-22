01d2e6a629e5a2c401fcb958e77787b1c0fbf287
Samvel Khachatryan <mr.samkhachatryan@gmail.com>
2020-06-22 19:00:35 +0400

    CHange

--- src/platform/services/enviroment.ts
+++ src/platform/services/enviroment.ts
@@ -6,5 +6,4 @@ export default {
   BASE_URL: (process.env.RAZZLE_ENV || '').trim() === 'staging' ? "https://apistaging.ineed.am/" :  "https://api.ineed.am/"
   // BASE_URL: "http://localhost:4002/",
   // BASE_URL: "http://192.168.0.106:5000/",
-  // https://api.ineed.am/
 };
