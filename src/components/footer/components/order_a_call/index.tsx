af9f70b562860b2c84e86f804494b94dc684f27a
Samvel Khachatryan <mr.samkhachatryan@gmail.com>
2020-06-26 15:48:58 +0400

    Footer

---
+++ src/components/footer/components/order_a_call/index.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import Settings from 'platform/services/settings';
+
+import './style.scss';
+import Modal from 'components/modal';
+import LoaderContent from 'components/loader-content';
+
+const OrderACall = React.memo(() => {
+  const [open, setOpen] = React.useState<Boolean>(false);
+
+  return <>
+    <button
+      onClick={() => setOpen(true)}
+      className="P-footer-order-a-call-button G-main-button"
+    >{Settings.translations.order_a_call}</button>
+
+    {open && <Modal className="P-footer-order-a-call-modal" onClose={() => setOpen(false)}>
+      <h3 className="G-main-color G-text-center">{Settings.translations.enter_your_phone_number}</h3>
+      <form className="G-main-form">
+        <div>
+          <input
+            className="G-main-input"
+            placeholder={Settings.translations.phone_number}
+          />
+        </div>
+        <LoaderContent className="G-main-button" loading={false}>{Settings.translations.order}</LoaderContent>
+      </form>
+    </Modal>}
+  </>;
+});
+
+export default OrderACall;
---
+++ src/platform/constants/contact-info.tsx
@@ -0,0 +1,3 @@
+export const contactPhoneNumber = '+374 60700500';
+export const contactEmail = 'alfapharm@alfapharm.am';
+export const contactAddress = 'ՀՀ ք. Երևան, Շիրակի փ. 1/68';
