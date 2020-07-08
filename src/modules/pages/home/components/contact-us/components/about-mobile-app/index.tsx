09edfacd1e3e61151d24d7ccefbc49efe1dc2670
Samvel Khachatryan <mr.samkhachatryan@gmail.com>
2020-07-08 16:54:51 +0400

    icon change

--- src/modules/pages/home/components/contact-us/components/about-mobile-app/index.tsx
+++ src/modules/pages/home/components/contact-us/components/about-mobile-app/index.tsx
@@ -11,11 +11,11 @@ const AboutDelivery = React.memo(() => {
 
   return (
     <div onClick={() => setOpened(true)}>
-      <i className="icon-Group-5801" />
+      <i className="icon-Group-5791" />
       <h3>{Settings.translations.mobile_app}</h3>
 
       {opened && <Modal className="P-contact-us-about-modal" onClose={() => setOpened(false)}>
-        <i className="icon-Group-5801" />
+        <i className="icon-Group-5791" />
         <h3>{Settings.translations.mobile_app}</h3>
         <p>{Settings.translations.about_mobile_app_text}</p>      
         <div className="P-download-images">
