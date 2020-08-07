7d3fa11bcda3d5cb667fa7e7d23e9e1f9dc2214b
Sarkis Petrosyan <sarkis.petrosyan1993@gmail.com>
2020-08-07 17:20:16 +0400

    Small change

--- src/platform/services/enviroment.ts
+++ src/platform/services/enviroment.ts
@@ -3,7 +3,6 @@
 // };
 
 export default {
-  BASE_URL: (process.env.RAZZLE_ENV || '').trim() === 'staging' ? "https://apistaging.ineed.am/" :  "http://localhost:5011/"
-  // BASE_URL: "http://localhost:4002/",
+  BASE_URL: (process.env.RAZZLE_ENV || '').trim() === 'staging' ? "https://alfaback.abmdemo.me/" :  "https://alfaback.abmdemo.me/"
   // BASE_URL: "http://192.168.0.106:5000/",
 };
