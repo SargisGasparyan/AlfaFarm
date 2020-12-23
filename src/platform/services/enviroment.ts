01b808a8eed324c38582ad50890e5f1c52117c09
Sarkis Petrosyan <sarkis.petrosyan1993@gmail.com>
2020-12-23 18:44:10 +0400

    Change

--- src/platform/services/enviroment.ts
+++ src/platform/services/enviroment.ts
@@ -5,7 +5,6 @@
 import * as CONFIG from '../../../config.json';
 
 export default {
-  //BASE_URL: (process.env.RAZZLE_ENV || '').trim() === 'staging' ? "https://alfaback.abmdemo.me/" :  "http://localhost:5011/",
+  BASE_URL: (process.env.RAZZLE_ENV || '').trim() === 'staging' ? "https://alfaback.abmdemo.me/" :  "http://localhost:5011/",
   WHOLESALE: CONFIG.wholesale,
-  BASE_URL: "http://localhost:5011/",
 };
