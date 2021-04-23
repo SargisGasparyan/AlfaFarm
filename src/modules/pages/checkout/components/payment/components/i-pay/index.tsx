f914e67f9dadba0365922c3ffee062e14eb8a9d4
Hayk Malkhasyan <hayk.malkhasyan0798@gmail.com>
2021-04-23 16:01:59 +0400

    fix: ampersant

--- src/modules/pages/checkout/components/payment/components/i-pay/index.tsx
+++ src/modules/pages/checkout/components/payment/components/i-pay/index.tsx
@@ -22,7 +22,7 @@ const IPay = React.memo(() => {
   };
 
   const createCard = async () => {
-    const returnUrl = window.location.pathname + (window.location.search ?? '');
+    const returnUrl = window.location.pathname + (window.location.search ?? '?isCard=true');
     const res = await PaymentController.registerCard(returnUrl);
     if (res && res.success) {
       window.location.href = res.data.formUrl;
