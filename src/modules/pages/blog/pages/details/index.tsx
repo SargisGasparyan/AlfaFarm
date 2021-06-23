c8419bfbb009a6bec1a909bae34378b4ebe0a452
Ashot Karapetyan <akaryan9991@gmail.com>
2021-06-23 22:38:47 +0400

    Add meta tags in blog and news pages

--- src/modules/pages/blog/pages/details/index.tsx
+++ src/modules/pages/blog/pages/details/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { withRouter, RouteComponentProps } from 'react-router-dom';
-
+import { ReactSEOMetaTags } from 'react-seo-meta-tags'
 import generic from 'platform/decorators/generic';
 import ROUTES from 'platform/constants/routes';
 import { byRoute } from 'platform/decorators/routes';
@@ -39,14 +39,23 @@ class Details extends HelperComponent<RouteComponentProps<IRouteParams>, IState>
     const { data } = this.state;
 
     return data ? (
-      <section className="G-page P-blog-details-page">
-        {window.routerHistory.length > 2 && <i className="G-back-icon icon-Group-5529" onClick={this.goBack} />}
-        <div className="P-content">
-          {!!data.images.length && <Images data={data} />}
-          <Info data={data} />
-          <p className="P-description" dangerouslySetInnerHTML={{ __html: data.description }} />
-        </div>
-      </section>
+        <>
+          <ReactSEOMetaTags
+              website={{
+                  title:  data.title,
+                  description: data.shortDescription,
+                  language: 'en-US',
+                  image: data.images.length ? data.images[0].path : '',
+              }}/>
+          <section className="G-page P-blog-details-page">
+            {window.routerHistory.length > 2 && <i className="G-back-icon icon-Group-5529" onClick={this.goBack} />}
+            <div className="P-content">
+              {!!data.images.length && <Images data={data} />}
+              <Info data={data} />
+              <p className="P-description" dangerouslySetInnerHTML={{ __html: data.description }} />
+            </div>
+          </section>
+        </>
     ) : <PageLoader />;
   }
 };
